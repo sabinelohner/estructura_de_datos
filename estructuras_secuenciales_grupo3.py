2a8322a72fc58a0de7b31cbc63bf0a33d0e5a342
sabinelohner <99361782+sabinelohner@users.noreply.github.com>
2022-02-16 19:40:22 -0400

    Add files via upload

---
+++ estructuras_secuenciales_grupo3.py
@@ -0,0 +1,4 @@
+radio=float(input("Digite el radio: "))
+area= 3.14*radio*radio
+print(f"El area del circulo es: {area}")
+# grupo 3
---
+++ tipo_de_datos_grupo2.py
@@ -0,0 +1,4 @@
+pi=float(3.14)
+radio=(input("Escriba el radio del circulo: "))
+print("El area del circulo es: ",pi*(radio**2))
+# Grupo 2
