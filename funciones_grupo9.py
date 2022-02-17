2e49dd2b90cee10ce3cd797de1f348694ca93b66
sabinelohner <99361782+sabinelohner@users.noreply.github.com>
2022-02-16 20:06:40 -0400

    Add files via upload

---
+++ funciones_grupo9.py
@@ -0,0 +1,15 @@
+def maximo(a,b):
+    if x>y:
+        return x
+    else:
+        return y
+
+def minimo(a,b):
+    if x<y:
+        return x
+    else:
+        return y
+    
+x=int(input("Un número: "))
+y=int(input("Otro número: "))
+print(maximo,minimo)
