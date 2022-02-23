277940d372d5054e9d4b21c716deb27c1fedcf5d
sabinelohner <99361782+sabinelohner@users.noreply.github.com>
2022-02-23 19:09:26 -0400

    Add files via upload

---
+++ olimpiadas_ejercicio2.py
@@ -0,0 +1,7 @@
+n = int(input("Introduce un numero entero: "))
+suma=0
+
+for i in range(1,n+1):
+    suma=suma+i
+
+print("La suma es:",suma)
---
+++ olimpiadas_ejercicio3.py
@@ -0,0 +1,4 @@
+peso=input("Peso en kg: ")
+altura=input("Altura en m: ")
+imc=round((float(peso)/float(estatura))**2,2)
+print("Tu indice de masa corporal es",imc)
